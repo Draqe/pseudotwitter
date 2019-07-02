4f3471d181289d759c36cc5031f64ab18ff7d151
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-02 21:35:51 +0800

    bcrypt on password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :password
+	has_secure_password
 	validates :first_name, :last_name, presence:true
 	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
 	validates :password, :confirmation => true
 	validates_length_of :password, :in => 6..20, :on => :create
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :tweets
 	resources :users
  	root 'users#index'
+ 	get 'users/new' => 'users#new'
+ 	post 'users' => 'users#create'
 end
