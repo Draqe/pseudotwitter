470a12f246c4b7e40b121094a4693a6be7aa1bf9
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-02 18:09:20 +0800

    Adjusted Passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+	attr_accessor :password
 	validates :first_name, :last_name, presence:true
-	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
-	validates :password, presence: true, length: {minimum: 8} 
+	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+	validates :password, :confirmation => true
+	validates_length_of :password, :in => 6..20, :on => :create
 end
