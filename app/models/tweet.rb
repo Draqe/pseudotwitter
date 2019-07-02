6d83aed953c005f57e15ee5b51d4d1e6d701809e
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-02 16:19:48 +0800

    Validates user id as number

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
 class Tweet < ApplicationRecord
-	validates :user_id, presence: true
+	validates :user_id, presence: true, numericality: true
 	validates :text, presence: true, length: {maximum: 140}
 end
