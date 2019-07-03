24af439a49b438ed1ab8d938f6d10628363f279e
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-03 20:35:55 +0800

    remove hash rocket
    for consistency

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, :last_name, presence:true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
-  validates :password, :confirmation => true
+  validates :password, confirmation: true
   validates_length_of :password, :in => 6..20, :on => :create
 end
