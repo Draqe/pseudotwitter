82488931357db32079dbcaa4dfdf34ce83d47623
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-03 20:34:03 +0800

    Indent Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
 		end
 	end
 
-	private
+	rivate
 		def user_params
 			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-		end
+			end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	resources :users
- 	root 'users#index'
- 	get 'users/new' => 'users#new'
- 	post 'users' => 'users#create'
+  resources :users
+  root 'users#index'
+  get 'users/new' => 'users#new'
+  post 'users' => 'users#create'
 end
