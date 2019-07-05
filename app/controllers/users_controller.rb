b7ac49efe8cd4de17d14cdf83d0341d97ef1ff17
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-05 15:48:03 +0800

    Indent Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		@user.email.downcase!
-		if @user.save
-			flash[:notice] = "Account created successfully!"
-			redirect_to root_path
-		else 
-			flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
-			render 'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    @user.email.downcase!
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else 
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
+      render 'new'
+    end
+  end
 
-	rivate
-		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-			end
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
