6137ec77a519581d0118c667950776650b79e3c2
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-05 16:24:13 +0800

    style conformation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, :last_name, presence:true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
-  validates :password, confirmation: true
-  validates_length_of :password, :in => 6..20, :on => :create
+  validates :password, presence: true, confirmation: true, length: {in: 6..20}, on: :create
 end
