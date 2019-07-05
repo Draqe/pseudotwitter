fa71018d004810631a61d0635aa47938a25d371b
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-05 23:50:30 +0800

    Route Fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    redirect_to login_path, notice: "Logged out!"
+    redirect_to sessions_path, notice: "Logged out!"
   end
 end
