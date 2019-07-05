915ff34e0e1068b0e843e6093b1751db27072d9c
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-05 23:43:22 +0800

    Create Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id.to_s
+      redirect_to tweets_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to login_path, notice: "Logged out!"
+  end
+end
