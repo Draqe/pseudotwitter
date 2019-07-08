d40509d41ae7fe62f89d0470a6b1a43a1a425fd9
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-08 15:55:05 +0800

    Bug Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def new
     @user = User.new
+  end
+
+  def show
+    @tweets = Tweet.all
+    @user = User.find(params[:id])
   end
 
   def create
@@ -12,14 +14,14 @@ class UsersController < ApplicationController
     if @user.save
       flash[:notice] = "Account created successfully!"
       redirect_to root_path
-    else 
+    else
       flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
       render 'new'
     end
   end
 
 private
-  
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
