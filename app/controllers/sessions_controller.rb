da7990f3c7be112489944b56328fcce37b4b56e4
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-09 17:13:15 +0800

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id.to_s
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       render 'new'
