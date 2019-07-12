90e66e63af077f7a5f292436fd7dd1e7fd3a919a
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-12 17:43:00 +0800

    Temporary Abandon

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,24 +2,29 @@ class TweetsController < ApplicationController
 
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
-  def show
-    @tweet = Tweet.find(params[:id])
-    @user = User.find(params[:user_id])
-    @retweet = @tweet.retweets.new
-  end
-
   def new
+    @tweet = Tweet.find(params[:tweet_id])
     @user = User.find(params[:user_id])
-    @tweet = @user.tweets.new
+    @retweet = @user.retweets.new
   end
 
   def create
     @user = User.find(params[:user_id])
-    @tweet = @user.tweets.new(tweet_params)
-    if @tweet.save
-      redirect_to user_path(@user)
+    if :retweet
+      @tweet = Tweet.find(params[:tweet_id])
+      @retweet = @tweet.retweet.new(tweet_params)
+      if @retweet.save
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
     else
-      render :show
+      @tweet = @user.tweets.new(tweet_params)
+      if @tweet.save
+        redirect_to user_path(@user)
+      else
+        render 'users/show'
+      end
     end
   end
 
@@ -61,6 +66,6 @@ class TweetsController < ApplicationController
   end
 
   def tweet_params
-    params.require(set_type.to_sym).permit(:text, :type)
+    params.require(set_type.to_sym).permit(:type, :text)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   has_many :tweets, dependent: :destroy
-  has_many :retweets
+  has_many :retweets, class_name: 'Retweet'
   has_secure_password
   validates :first_name, :last_name, presence:true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
