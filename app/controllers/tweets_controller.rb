62ba0b23d3a6b4e1a96eea3a1b1dd250dc1874ec
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-16 20:31:16 +0800

    Revamp views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,11 @@ class TweetsController < ApplicationController
 
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
+  def show
+  end
+
   def new
+    @user = current_user
     @tweet = Tweet.find(params[:tweet_id])
     @reply = @tweet.retweets.new
     @reply.type = params[:type]
@@ -11,7 +15,6 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user
-
     if @tweet.save
       redirect_to user_path(current_user)
     else
