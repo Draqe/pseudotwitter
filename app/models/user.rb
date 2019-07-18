48281a5f94236dc29e50d5f4b3b75726d71dddc7
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-18 18:19:38 +0800

    password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :replies, class_name: 'Reply'
   has_secure_password
+
+  password_format = /\A
+    (?=.{8,})
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+    (?=.*[[:^alnum:]])
+  /x
+
   validates :first_name, :last_name, presence:true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: {in: 6..20}, on: :create
+  validates :password, presence: true, confirmation: true, format: { with: password_format }, on: :create
 end
