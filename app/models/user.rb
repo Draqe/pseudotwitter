2de58be2e766385d36fb2544bf70ac26f134006a
Don Richson Que <33984642+Draqe@users.noreply.github.com>
2019-07-26 14:03:31 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,9 @@ class User < ApplicationRecord
   has_many :replies, class_name: 'Reply'
   has_secure_password
 
-  password_format = /\A
-    (?=.{8,})
-    (?=.*\d)
-    (?=.*[a-z])
-    (?=.*[A-Z])
-    (?=.*[[:^alnum:]])
-  /x
+  PASSWORD_FORMAT = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x
 
   validates :first_name, :last_name, presence:true
   validates :email, format: {with: /.+@.+\..+/i, message: 'only valid emails allowed'}, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, format: { with: password_format}, on: :create
+  validates :password, presence: true, confirmation: true, format: { with: PASSWORD_FORMAT}, on: :create
 end
