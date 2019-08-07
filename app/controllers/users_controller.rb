b5628f2700b87aebe6fb29f25e3cce10f850781a
Don Richson Que <soulstryker007@gmail.com>
2019-08-07 16:50:17 +0800

    Root Page improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.email.downcase!
     if @user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render 'new'
     end
